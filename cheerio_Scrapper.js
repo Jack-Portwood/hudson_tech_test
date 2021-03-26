09526fcd08230fc92dbf1aed923a137602338467
Jack Portwood <Jack.Portwood14@gmail.com>
2021-03-26 17:15:16 +0000

    bug fixed

--- cheerio_Scrapper.js
+++ cheerio_Scrapper.js
@@ -1,56 +1,48 @@
-const request = require ("request");
-const cheerio = require ("cheerio");
-const jsonfile = require ("jsonfile");
+const request = require("request");
+const cheerio = require("cheerio");
+const jsonfile = require("jsonfile");
 
 //request to webPage will error handling, html set as variable
 request("https://dev-test.hudsonstaging.co.uk/", (error, response, html) => {
-    if(!error && response.statusCode === 200){
-        const webPage = cheerio.load(html);
+  if (!error && response.statusCode === 200) {
+    const webPage = cheerio.load(html);
 
-        //new array to push reterived data into
-        const products = [];
+    //new array to push reterived data into
+    const products = [];
 
-        //loops through data to create metadate object which is pushed into products array
-        webPage(".product-tile").each((index, element) => {
-          //gets image data
-          let img_url = webPage(element).find("img").attr("src");
+    //loops through data to create  object which is pushed into products array
+    webPage(".product-tile").each((index, element) => {
+      //gets name of product
+      let product = webPage(element).find(".product-name").text().trim();
 
-          //gets quatiaty data removes extra letters/ whitespace and converts string to Int
-          let qauntity = webPage(element)
-            .find(".details p")
-            .first()
-            .text()
-            .trim()
-            .slice(10);
-            qauntity = parseInt(qauntity);
+      //gets image data
+      let img_url = webPage(element).find("img").attr("src");
 
-          //gets price data removes extra letters/ whitespace and converts string to Int
-          let price = webPage(element)
-            .find(".details p")
-            .last()
-            .text()
-            .trim()
-            .slice(8);
-            price = parseInt(price);
+      //gets quatiaty data removes extra letters/ whitespace and converts string to Int
+      let quantity = webPage(element)
+        .find(".details p")
+        .first()
+        .text()
+        .trim()
+        .slice(10)
+        quantity = parseInt(quantity);
 
-          //pushes items it array as metadata object
-          products[index] = metadata = { img_url, qauntity, price };
-        });
+      //gets price data removes extra letters/ whitespace and converts string to Int
+      let price = webPage(element)
+        .find(".details p")
+        .last()
+        .text()
+        .trim()
+        .slice(8)
+        price = parseFloat(price);
+      //pushes items it array as metadata object
+      product = { "product": product, "metadata": { "img_url": img_url, "quantity": quantity, "price": price }};
 
-        //crates second object containing product name removes white space. 
-        webPage(".product-tile").each((index, element)=> {
-            const product = webPage(element)
-            .find(".product-name")
-            .text()
-            .trim();
+      products.push(product);
 
-            //overwrites products array passing both pardouctName object and metadata object 
-            products[index] = {product, metadata};
-
-        });
-        //converts data to Json file
-        jsonfile.writeFile("data.json", products)
-
-
-    }
+    });
+    console.log(products)
+    //converts data to Json file
+    jsonfile.writeFile("data.json", products);
+  }
 });
